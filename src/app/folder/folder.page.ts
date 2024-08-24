9bc0d9d391cab392406b9aa01e9fcde4727546d6
Cursos FIC <FIC@educ501.local>
2024-08-24 16:38:53 -0300

    primeiro commit

---
+++ src/app/folder/folder.page.ts
@@ -0,0 +1,33 @@
+import { Component, inject, OnInit, ViewChild } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { IonModal } from '@ionic/angular';
+import { OverlayEventDetail } from '@ionic/core/components';
+
+@Component({
+  selector: 'app-folder',
+  templateUrl: './folder.page.html',
+  styleUrls: ['./folder.page.scss'],
+})
+export class FolderPage implements OnInit {
+  @ViewChild(IonModal) modal!: IonModal;
+  message: string = '';
+  name: string = '';
+
+  ngOnInit() {
+
+  }
+  cancel() {
+    this.modal.dismiss(null, 'cancel');
+  }
+
+  confirm() {
+    this.modal.dismiss(this.name, 'confirm');
+  }
+
+  onWillDismiss(event: Event) {
+    const ev = event as CustomEvent<OverlayEventDetail<string>>;
+    // if (ev.detail.role === 'confirm') {
+    //   this.message = `Hello, ${ev.detail.data}!`;
+    // }
+  }
+}
